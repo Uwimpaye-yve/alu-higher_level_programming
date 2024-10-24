c1d1198fc4b4221168e33a75c584c43f90a17f82
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-10-24 15:32:07 +0200

    new file

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    if chr(c) != 'q' and chr(c) != 'e':
-        print("{}".format(chr(c)), end="")
+print("{}".format("".join(
+    "{:c}".format(i) for i in range(97, 123) if i not in (101, 113)
+)), end="")
