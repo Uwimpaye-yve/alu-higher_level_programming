06c37d2425b0192c3eb88125846d601e688e120f
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-10-25 10:44:41 +0200

    debug

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
+# 4-hidden_discovery.py
+import hidden_4
 
-    names = sorted(name for name in dir(hidden_4) if not name.startswith("__"))
-    for name in names:
+# Print only names that do not start with an underscore
+for name in dir(hidden_4):
+    if not name.startswith("_"):
         print(name)
