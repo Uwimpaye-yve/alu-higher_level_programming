6a700902cc018974f56c6c9c6a54591cac845ae1
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-10-26 21:01:02 +0200

    debug

---
+++ python-import_modules/hidden_4.py
@@ -0,0 +1,16 @@
+# hidden_4.py
+
+def my_secret_santa():
+    return "Santa is secret!"
+
+def print_hidden():
+    print("This is a hidden function.")
+
+def print_school():
+    print("Welcome to Holberton School!")
+
+def test_betty():
+    print("Testing Betty.")
+
+if __name__ == "__main__":
+    pass
