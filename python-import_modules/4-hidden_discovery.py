3528b1f2122b7464d1c1c1a5cf1052f375176f5a
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-10-26 20:48:10 +0200

    debug

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
+import hidden_4
 
+def print_names():
     names = sorted(name for name in dir(hidden_4) if not name.startswith("__"))
     for name in names:
         print(name)
+
+if __name__ == "__main__":
+    print_names()
