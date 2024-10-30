4c2732169211ae1c3c698845e4400a3c1ee44a85
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-10-30 12:06:01 +0200

    Add 0file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
---
+++ python-exceptions/README.md
@@ -0,0 +1,1 @@
+This is python project on exceptions and errors
