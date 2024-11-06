5e7fa6ce6f2e7e2d315732d973290e940c23d4ef
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-11-06 18:51:17 +0200

    New file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+
+""" Defines lookup function """
+
+
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
+    """ Returns the list of attributes and methods of an object """
     return dir(obj)
