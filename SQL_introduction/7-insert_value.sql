458255129a27dc703eda865bfb53fccd2cac0744
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-11-14 17:16:34 +0200

    remaining files

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- inserting rows
-INSERT INTO first_table(id, name) VALUES (89, 'BEST SCHOOL');
+-- Inserts a new row in a table
+-- Query to insert a new row in the table first_table
+INSERT INTO first_table (id, name) VALUES (89, "Best School")
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
--- displaying number of records
-SELECT COUNT(*) from first_table WHERE id = 89;
+-- Count 89
+-- displays the number of records with id = 89 in the table
+SELECT COUNT(*) FROM first_table WHERE id=89;
