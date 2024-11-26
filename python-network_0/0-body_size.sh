7e7c2a2506094487bd3ebd946e5cd06ae852d558
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-11-26 08:59:16 +0200

    debugging

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL using curl and displays the size of the body of the response in bytes
-curl -s $1 | wc -c
+curl -so /dev/null -w '%{size_download}\n' $1
