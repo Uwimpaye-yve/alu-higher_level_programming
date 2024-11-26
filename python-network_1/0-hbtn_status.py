1ffd532585f74f32700b7cb50ec4c8f8768e039b
Uwimpaye-yve <y.uwimpaye@alustudent.com>
2024-11-26 09:20:11 +0200

    debug

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""  fetches https://alu-intranet.hbtn.io/status  """
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
+
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-if url.startswith('https://'):
-    url = "https://alu-intranet.hbtn.io/status"
+
+def main():
+    """Fetches url and displays the body of the response"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
 
 if __name__ == "__main__":
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
-        print("\t- utf8 content:", content.decode("utf-8"))
+    main()
